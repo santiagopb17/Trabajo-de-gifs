c15f62f6845a8fb66c4eabae13cf6a1ac6f7ea6f
Santiago Perez <santiago.perez@sophossolutions.com>
2021-07-22 14:57:05 -0500

    Trabajo de gifs

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+  private apiKey: string = '7tT4qErRoBYkyQTWU9E3Qk3zG5U2Ki7c';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _historial : string []= [];
+  public resultados: Gif[]= [];
+
+
+  get historial(){
+    
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient){ 
+    
+    this._historial=JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados=JSON.parse(localStorage.getItem('resultados')!) || []; 
+    
+  }
+
+  buscarGifs(query:string){
+
+    query=query.trim().toLowerCase();
+    
+    if( !this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial=this._historial.splice(0,10);
+      localStorage.setItem('historial',JSON.stringify(this._historial));
+    }  
+    
+    const params = new HttpParams().set('api_key', this.apiKey).set('limit', '20').set('q',query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params}).
+    subscribe(resp =>{
+      this.resultados=resp.data;      
+      localStorage.setItem('resultados',JSON.stringify(this.resultados));
+    })
+  }
+
+}
